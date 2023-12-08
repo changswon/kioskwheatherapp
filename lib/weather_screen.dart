9544a92b5cee5d275950bef6d8951d444e41da71
changswon <changs1505@gmail.com>
2023-12-08 17:21:27 +0900

    1211

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData}); //생성자
@@ -18,35 +22,89 @@ class _WeatherScreenState extends State<WeatherScreen> {
     updateData(widget.parseWeatherData);
   }
 
-  void updateData(dynamic weatherData){
-        double temp2 = weatherData['main']['temp'];
-        temp = temp2.toInt();
-        cityName = weatherData['name'];
+  void updateData(dynamic weatherData) {
+    double temp2 = weatherData['main']['temp'];
+    temp = temp2.toInt();
+    cityName = weatherData['name'];
 
-        print(temp);
-        print(cityName);
+    print(temp);
+    print(cityName);
   }
 
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:mm a").format(now);
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              '$cityName',
-              style: TextStyle(
-                fontSize: 30.0
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        //title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          color: Colors.white,
+          onPressed: (){},
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.location_searching,
+                color: Colors.white,
               ),
+              onPressed: (){},
+              iconSize: 30.0,
+          )
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+               'image/background.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '$temp',
-              style: TextStyle(
-                fontSize: 30.0
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 150.0,
+                  ),
+                  Text(
+                    'Ulsan',
+                    style: GoogleFonts.lato(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      TimerBuilder.periodic(
+                        (Duration(minutes: 1)),
+                        builder:(context){
+                          print('${getSystemTime()}',);
+                          return Text(
+                            '${getSystemTime()}',
+                            style: GoogleFonts.lato(
+                                fontSize: 16.0,
+                                color: Colors.white
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
